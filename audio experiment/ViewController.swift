b2310ec3572e0def87782d07eee897b20d717a46
Yoshua Elmaryono <yoshuaelmaryono@yoshua-e.local>
2018-07-16 21:01:33 +0700

    add bgm player

--- audio experiment/ViewController.swift
+++ audio experiment/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var bgmPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(999)
+        loadPlayerForBGM()
+        bgmPlayer.play()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func loadPlayerForBGM(){
+        guard let backgroundMusicData = NSDataAsset(name: "bgm")?.data else { return }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            /* The following line is required for the player to work on iOS 11. */
+            bgmPlayer = try AVAudioPlayer(data: backgroundMusicData, fileTypeHint: AVFileType.mp3.rawValue)
+            /* iOS 10 and earlier require the following line:
+             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
 }
 
